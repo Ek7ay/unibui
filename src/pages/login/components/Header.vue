b423ef9a2c922385ae52ca4628a8fbaa65732ca8
EK <qxjj7ay@163.com>
2019-04-25 18:35:11 +0800

    'element'

--- src/pages/login/components/Header.vue
+++ src/pages/login/components/Header.vue
@@ -1,15 +1,37 @@
 <template>
     <div class="header">
-
+      <img class="img1" src="../../../assets/img/login/liantongLogo.png" height="48" width="88"/>
+      <img class="img2" src="../../../assets/img/login/Group.png" height="21" width="252"/>
+      <i class="el-icon-edit"></i>
+      <i class="el-icon-toilet-paper"></i><i class="el-icon-toilet-paper"></i><i class="el-icon-toilet-paper">
+      </i><i class="el-icon-toilet-paper"></i>
+      <i class="el-icon-toilet-paper">
+      </i><i class="el-icon-toilet-paper"></i><i class="el-icon-toilet-paper"></i>
     </div>
 </template>
 
 <script>
     export default {
-        name: "LoginHeader"
+        name: "LoginHeader",
+        data () {
+          return{
+            from: {
+              name: ""
+            }
+          }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+.header
+  width 100%
+  height 50px
+  display flex
+  align-items center
+  border 1px solid red
+
+
+
 
 </style>
