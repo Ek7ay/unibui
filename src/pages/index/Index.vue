e405ab1ae3c3ff00a3603df977ccd8b66eb18f8b
EK <qxjj7ay@163.com>
2019-04-28 23:49:13 +0800

    'menu'

--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
       <index-header></index-header>
+      <index-menu></index-menu>
     </div>
 </template>
 
 <script>
     import IndexHeader from './components/Header'
+    import IndexMenu from './components/Menu'
 
     export default {
         name: "Index",
         components: {
-          IndexHeader
+          IndexHeader,
+          IndexMenu
         }
     }
 </script>
---
+++ src/pages/index/components/Menu.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="main">
+        <el-col :span="24">
+            <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b"
+            >
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>导航一</span>
+                    </template>
+                   
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+               
+
+                    <el-submenu index="1-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="1-4-1">选项1</el-menu-item>
+                    </el-submenu>
+
+                </el-submenu>
+
+                <el-submenu index="2">
+                    <template slot="title">
+                        <i class="el-icon-menu"></i>
+                        <span>导航2</span>
+                    </template>
+                    <el-menu-item index="2-1" disabled="">选择1</el-menu-item>
+                    <el-menu-item index="2-2">选择2</el-menu-item>
+                </el-submenu>
+
+                <el-menu-item index="3">
+                    <i class="el-icon-document"></i>
+                    <span slot="title">导航三</span>
+                </el-menu-item>
+
+                <el-menu-item index="4">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">导航四</span>
+                </el-menu-item>
+            </el-menu>
+        </el-col>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu",
+        methods: {
+            handleOpen (key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose (key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .main
+        width 245px
+        margin-top -1px
+        overflow auto
+        height 100vh
+        background #867373
+
+    
+</style>
